80075baf526c0aab360782308dd177a0080bec91
Erik Roque <67030826+erikroque@users.noreply.github.com>
2025-06-20 11:24:04 -0300

    voltando ao que era antes

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-
-console.log("removi o escrevendo mais uma linha")
-console.log("Escrevi mais um código na linha 4 ")
-
-/*Vou deixar um comentário aqui*/
