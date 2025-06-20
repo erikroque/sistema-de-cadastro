92e7c77379628be9c0fd24ef47d27fbba929d58c
erikroque <erikroquesud@hotmail.com>
2025-06-20 11:48:29 -0300

    voltei atrás

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+
+console.log("removi o escrevendo mais uma linha")
+console.log("Escrevi mais um código na linha 4 ")
+
+/*Vou deixar um comentário aqui*/
